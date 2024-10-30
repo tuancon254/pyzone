e24e1ba3a6563a5240c0756f949a4a285b5b62cc
tuancon254 <tuancon254@gmail.com>
2024-10-30 23:12:20 +0700

    update

--- app/database.py
+++ app/database.py
@@ -5,7 +5,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL')
+SQLALCHEMY_DATABASE_URL = os.environ('DATABASE_URL')
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
