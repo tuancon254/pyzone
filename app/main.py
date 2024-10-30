9c697db9eddd54dd16b86b7161b32452c5b2a31a
tuancon254 <tuancon254@gmail.com>
2024-10-31 00:11:20 +0700

    update

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
-from app.models import Base
-from app.database import engine
-from api.routes.auth import auth
 # Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
-app.include_router(auth)
+@app.get("/hello")
+async def hello():
+    return {"mesage": "hello"}
--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, DateTime, Boolean
-from app.database import Base
+from database import Base
 from datetime import datetime
 
 class User(Base):
